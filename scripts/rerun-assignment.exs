f3a413340219e82e58d9ba00aad9fba917f980b8
Nat Tuck <nat@ferrus.net>
2021-03-03 12:07:53 -0500

    update

--- scripts/rerun-assignment.exs
+++ scripts/rerun-assignment.exs
@@ -3,6 +3,7 @@ alias Inkfish.Subs
 alias Inkfish.Assignments
 alias Inkfish.Container.Job
 alias Inkfish.Uploads.Upload
+alias Inkfish.Repo
 
 defmodule A do
   def listen do
@@ -16,33 +17,6 @@ defmodule A do
     IO.inspect({:p2, a, b})
   end
 
-  def autograde(grade_id) do
-      key = Inkfish.Text.zeropad(grade_id, 8)
-      job = %Job{
-        key: key,
-        uuid: grade.log_uuid,
-        container: %{
-          base: "debian:buster",
-          packages: ["clang", "clang-tools", "valgrind"],
-          user_commands: [
-            "curl https://sh.rustup.rs -sSf | sh -s -- -y",
-          ],
-          size_limit: "10M",
-        },
-        driver: %{
-          script: "classic",
-          SUB: Upload.upload_url(grade.sub.upload),
-          GRA: Upload.upload_url(grade.grade_column.upload),
-        },
-      }
-
-      rv = Inkfish.Container.Queue.add(job, &A.print2/2)
-      IO.inspect(rv)
-
-      Inkfish.Itty.open(grade.log_uuid)
-      A.listen
-  end
-
   def nil_script?(sub) do
     Enum.all? sub.grades, fn gr ->
       gr.score == nil
@@ -50,31 +24,28 @@ defmodule A do
   end
 end
 
+Inkfish.Container.Queue.start
+
 [arg1] = System.argv()
 
 {asg_id, _} = Integer.parse(arg1)
 
 
 asg = Assignments.get_assignment!(asg_id)
+      |> Repo.preload([subs: [:grades, reg: :user]])
 
-Enum.each asg.subs, fn sub ->
-  sub = Subs.get_sub!(sub_id)
-  if sub.active && A.nil_script?(sub) do
-    Enum.each asg.grade_columns, fn gcol ->
-      if gcol.kind == "script" do
-        uuid = Inkfish.Text.gen_uuid()
-        attrs = %{
-          grade_column_id: gcol.id,
-          sub_id: sub.id,
-          log_uuid: uuid,
-        }
-        {:ok, grade} = Grades.create_grade(attrs)
+subs = asg.subs
+       |> Enum.filter(&(&1.active && A.nil_script?(&1)))
 
-        grade = Grades.get_grade_for_autograding!(grade.id)
-        IO.inspect(grade)
-
-        A.autograde(grade.id)
-      end
-    end
-  end
+Enum.each Enum.take(subs, 2), fn sub ->
+  hash = hd(Inkfish.Subs.autograde!(sub))
+  IO.inspect({sub.reg.user.email, hash})
+  Inkfish.Itty.open(hash)
+  |> IO.inspect
+  A.listen
 end
+
+queue = Inkfish.Container.Queue.list()
+        |> Enum.filter(&(&1.idx != nil))
+
+IO.inspect({:queue, length(queue)})
